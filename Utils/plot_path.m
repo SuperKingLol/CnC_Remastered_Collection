6219a3b0810f606af27d01ad8f2eb3a2892d5441
Unknown <laurence.stark11@gmail.com>
2020-06-11 23:45:00 +0100

    Add Octave/MATLAB function to plot debug pathfinding data

---
+++ Utils/plot_path.m
@@ -0,0 +1,15 @@
+## Author: SuperKingLol
+## Created: 2020-06-11
+
+% Load the map and path CSV files and plot their outputs. This function is 
+% only intended for use to assist debugging and developing pathfinding in C&C.
+function plot_path(final_path_file, map_file)
+  map_vector = csvread(map_file);
+  path = csvread(final_path_file);
+  
+  % Reshape map vector into correctly oriented 2D RGB image data
+  map = permute(reshape(map_vector, [64, 64, 3]), [2,1,3]);
+  imshow(map);
+  hold on;
+  plot(path(:,1) + 1, path(:,2) + 1,'b');
+endfunction
